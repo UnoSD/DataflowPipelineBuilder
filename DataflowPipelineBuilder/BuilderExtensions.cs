77ad031ff575077c4375e2a16956113ad3b23538
Michele Cantelli <emmekappa@gmail.com>
2019-03-10 19:53:44 +0100

    Adding extension methods that supports DataflowBlockOptions while using LINQ-like extension syntax.

--- DataflowPipelineBuilder/BuilderExtensions.cs
+++ DataflowPipelineBuilder/BuilderExtensions.cs
@@ -13,11 +13,22 @@ namespace DataflowPipelineBuilder
             this IBuilder<TOrigin, T> builder,
             int size
         ) => builder.Then(new BatchBlock<T>(size));
+        
+        public static IBuilder<TOrigin, IReadOnlyCollection<T>> Batch<TOrigin, T>
+        (
+            this IBuilder<TOrigin, T> builder,
+            int size, GroupingDataflowBlockOptions dataflowBlockOptions
+        ) => builder.Then(new BatchBlock<T>(size, dataflowBlockOptions));
 
         public static IBuilder<TOrigin, T> SelectMany<TOrigin, T>
         (
             this IBuilder<TOrigin, IEnumerable<T>> builder
         ) => builder.Then(new TransformManyBlock<IEnumerable<T>, T>(enumerable => enumerable));
+        
+        public static IBuilder<TOrigin, T> SelectMany<TOrigin, T>
+            (
+            this IBuilder<TOrigin, IEnumerable<T>> builder, ExecutionDataflowBlockOptions dataflowBlockOptions
+        ) => builder.Then(new TransformManyBlock<IEnumerable<T>, T>(enumerable => enumerable, dataflowBlockOptions));
 
         public static IBuilder<TOrigin, TResult> Select<TOrigin, T, TResult>
         (
@@ -25,6 +36,12 @@ namespace DataflowPipelineBuilder
             Func<T, TResult> selector
         ) => builder.Then(new TransformBlock<T, TResult>(selector));
 
+        public static IBuilder<TOrigin, TResult> Select<TOrigin, T, TResult>
+        (
+            this IBuilder<TOrigin, T> builder,
+            Func<T, TResult> selector, ExecutionDataflowBlockOptions dataflowBlockOptions
+        ) => builder.Then(new TransformBlock<T, TResult>(selector, dataflowBlockOptions));
+        
         // This will buffer all the data and call complete
         // what happens if at this stage the other blocks
         // are not linked yet?
